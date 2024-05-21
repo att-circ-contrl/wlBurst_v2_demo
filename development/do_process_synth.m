296212d4785ff6abfca20dcc444f79cab3a5cd2b
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-05-21 10:14:44 -0500

    Added data extraction for wlBurst synthetic data. Added cooked wlBurst dataset.

---
+++ development/do_process_synth.m
@@ -0,0 +1,15 @@
+% wlBurst demo - Development scripts - Data processing for wlBurst synthetic.
+% Written by Christopher Thomas.
+
+
+% The dataset is already in FT format and small enough. Just copy it.
+
+load([ '..' filesep 'datasets-raw' filesep 'minimal-synthevents.mat' ]);
+
+ftdata_wlburst = synthdata;
+
+save( [ 'output' filesep 'ftdata-wlburst.mat' ], '-v7.3', 'ftdata_wlburst' );
+
+
+%
+% This is the end of the file.
